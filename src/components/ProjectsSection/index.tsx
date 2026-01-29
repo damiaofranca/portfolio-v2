c05c983e8a56b1555cff0f2eb5d920a58b404cc2
Damião da Silva França <77007541+damiaofranca@users.noreply.github.com>
2026-01-29 17:18:25 -0300

    fix: ajustando porfolio

--- src/components/ProjectsSection/index.tsx
+++ src/components/ProjectsSection/index.tsx
@@ -16,11 +16,7 @@ import {
 } from "./styles";
 
 import Domino from "@/assets/photos/domino.png";
-import Pokedex from "@/assets/photos/pokedex.png";
-import Vertigo from "@/assets/photos/vertigo.png";
 import NpmLogo from "@/assets/photos/npm_logo.jpg";
-import InProgress from "@/assets/photos/progress_in.svg";
-import LandingPage from "@/assets/photos/landing-page.png";
 import ClientVysor from "@/assets/photos/client-vysor.png";
 import ActionCard from "../ActionCard";
 
@@ -38,33 +34,18 @@ const ProjectsSection: React.FC<IProjectsSection> = ({ texts }) => {
 			urlProject: "https://dominoes-iota.vercel.app/",
 		},
 		{
-			image: { url: NpmLogo, alt: texts.list.four.altImage },
-			title: texts.list.four.title,
-			description: texts.list.four.description,
-			technologies: texts.list.four.stack,
-			urlProject: "https://www.npmjs.com/package/google-handler-area-react",
-		},
-		{
-			image: { url: ClientVysor, alt: texts.list.second.altImage },
+			image: { url: NpmLogo, alt: texts.list.second.altImage },
 			title: texts.list.second.title,
 			description: texts.list.second.description,
 			technologies: texts.list.second.stack,
-			urlProject: "https://client-vysor.vercel.app",
+			urlProject: "https://www.npmjs.com/package/google-handler-area-react",
 		},
 		{
-			image: { url: LandingPage, alt: texts.list.first.altImage },
+			image: { url: ClientVysor, alt: texts.list.first.altImage },
 			title: texts.list.first.title,
 			description: texts.list.first.description,
 			technologies: texts.list.first.stack,
-			urlProject: "https://landing-page-fintech-demo.vercel.app/",
-		},
-
-		{
-			image: { url: Vertigo, alt: texts.list.five.altImage },
-			title: texts.list.five.title,
-			description: texts.list.five.description,
-			technologies: texts.list.five.stack,
-			urlProject: "https://vertigo-demo.vercel.app/",
+			urlProject: "https://client-vysor.vercel.app",
 		},
 	];
 
